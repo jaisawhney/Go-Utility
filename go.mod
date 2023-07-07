103c4caba4ba5eb9438d2e797b4aadb6d10439b4
Jai Sawhney <jai.sawhney1@gmail.com>
2023-07-07 13:36:06 -0700

    Scrape html from a website

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module github.com/jaisawhney/Go-Utility
 
 go 1.20
+
+require (
+	github.com/JohannesKaufmann/html-to-markdown v1.4.0
+	github.com/chromedp/cdproto v0.0.0-20230625224106-7fafe342e117
+	github.com/chromedp/chromedp v0.9.1
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.2.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
 
+	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	website := flag.String("website", "", "Website to download")
+
+	flag.Parse()
+
+	if *website != "" {
+		var html string
+		chromedp.Run(ctx,
+			chromedp.Navigate(*website),
+			chromedp.ActionFunc(func(ctx context.Context) error {
+				node, err := dom.GetDocument().Do(ctx)
+				if err != nil {
+					return err
+				}
+				html, err = dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
+				return err
+			}),
+		)
+		fmt.Println(html)
+	} else {
+		fmt.Println("'--website' flag missing")
+	}
 }
