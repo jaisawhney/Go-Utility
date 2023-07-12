60f30b38ebcb11cc45ae8f415fa0804ea6f0d800
Jai Sawhney <jai.sawhney1@gmail.com>
2023-07-11 17:06:18 -0700

    Save the website as a markdown file

---
+++ README.md
@@ -0,0 +1,4 @@
+# Go Utility
+
+[![Go Report Card](https://goreportcard.com/badge/github.com/jaisawhney/Go-Utility)](https://goreportcard.com/report/github.com/jaisawhney/Go-Utility)
+
--- main.go
+++ main.go
@@ -4,33 +4,62 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"os"
 
-	"github.com/chromedp/cdproto/dom"
 	"github.com/chromedp/chromedp"
+
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/JohannesKaufmann/html-to-markdown/plugin"
 )
 
-func main() {
+// Navigates to and fetches the HTML for the provided website
+func fetchHtml(website string) string {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
+	var html string
+	chromedp.Run(ctx,
+		chromedp.Navigate(website),
+		chromedp.OuterHTML("body", &html, chromedp.ByQuery),
+	)
+	return html
+}
+
+// Processes the HTML markup and converts it to a markdown string
+func createMarkdown(baseDomain string, html string) string {
+	converter := md.NewConverter(baseDomain, true, nil)
+	converter.Use(plugin.Table())
+	converter.Use(plugin.TaskListItems())
+
+	markdown, err := converter.ConvertString(html)
+	if err != nil {
+		panic(err)
+	}
+	return markdown
+}
+
+// Saves the new markdown file
+func saveFile(baseDomain string, markdown string, filePath string) {
+	outputFile, err := os.Create("./output/" + baseDomain + ".md")
+	if err != nil {
+		panic(err)
+	}
+
+	outputFile.Write([]byte(markdown))
+	fmt.Println("Saved to '" + baseDomain + ".md'")
+}
+
+func main() {
 	website := flag.String("website", "", "Website to download")
 
 	flag.Parse()
 
 	if *website != "" {
-		var html string
-		chromedp.Run(ctx,
-			chromedp.Navigate(*website),
-			chromedp.ActionFunc(func(ctx context.Context) error {
-				node, err := dom.GetDocument().Do(ctx)
-				if err != nil {
-					return err
-				}
-				html, err = dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
-				return err
-			}),
-		)
-		fmt.Println(html)
+		var html = fetchHtml(*website)
+		var baseDomain = md.DomainFromURL(*website)
+		var markdown = createMarkdown(baseDomain, html)
+
+		saveFile(baseDomain, markdown, markdown)
 	} else {
 		fmt.Println("'--website' flag missing")
 	}
